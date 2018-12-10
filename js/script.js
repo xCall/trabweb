527f055fc0f1f3fcd194c04e50ee4d44f4dc33e0
CARLOS DANIEL <carlosfaita99@gmail.com>
2018-12-10 20:56:50 -0200

    Section com produtos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $(function () {
     $('[data-toggle="popover"]').popover()
-  })
+  }
+)
+
+function colorir(){
+  document.getElementById('menu');
+  document.getElementById('top-nav-collapse').style.backgroundColor = "#33B5E5";
+}
